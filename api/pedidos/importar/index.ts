dc0c8a2a4d56168fa4fa8fd9c84035fa5c4e2ad6
rockstaraki2-byte <rockstaraki2@gmail.com>
2026-09-09 09:23:05 -0300

    fix: use runtime JS import for order import endpoint

--- api/pedidos/importar/index.ts
+++ api/pedidos/importar/index.ts
@@ -1,4 +1,4 @@
-import { handleOrderImportHttp } from "../../_lib/orderImportHttp.ts";
+import { handleOrderImportHttp } from "../../_lib/orderImportHttp.js";
 
 export default async function handler(req: any, res: any) {
   return handleOrderImportHttp(req, res, false);
